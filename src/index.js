767dd4c9ce5fb4851d37cea4e2c11a776a8b5033
AfterThreeYears <470326964@qq.com>
2019-03-29 10:49:24 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,84 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+function renderTitle(newTitle, oldTitle) {
+  if (newTitle === oldTitle) return;
+  console.log('renderTitle');
+  const t = document.querySelector('#title');
+  t.innerHTML = newTitle.text;
+  t.style.color = newTitle.color;
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function renderContent(newContent, oldContent) {
+  if (newContent === oldContent) return;
+  console.log('renderContent');
+  const c = document.querySelector('#content');
+  c.innerHTML = newContent.text;
+  c.style.color = newContent.color;
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+function render(newState, oladState) {
+  if (newState === oladState) return;
+  debugger;
+  console.log('render');
+  renderTitle(newState.title, oladState.title);
+  renderContent(newState.content, oladState.content);
+}
+
+const appState = {
+  title: {
+    text: '炉石传说',
+    color: 'red',
+  },
+  content: {
+    text: '祖尔金',
+    color: 'blue',
+  },
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case 'UPDATE_TITLE':
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          text: action.payload
+        },
+      };
+    case 'UPDATE_CONTENT':
+      return {
+        ...state,
+        content: {
+          ...state.content,
+          text: action.payload
+        },
+      };
+    default:
+      return state;
+  }
+} 
+
+function createStore(state, reducer) {
+  const listeners = [];
+  const subscribe = (listener) => listeners.push(listener);
+  const getState = () => state;
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  }
+  return { getState, dispatch, subscribe };
+}
+
+const { dispatch, subscribe, getState } = createStore(appState, reducer);
+window.dispatch = dispatch;
+
+let oldState = getState();
+subscribe(() => {
+  const newState = getState();
+  console.log(newState, oldState);
+  render(newState, oldState);
+  oldState = newState;
+});
+
+render(getState(), {});
+dispatch({ type: 'UPDATE_TITLE', payload: '圣盾' });
+
+
